de4ea232d03e78a2c1698fc6fe6d57b817c7629a
anikait <anikait.gupta2024@gmail.com>
2025-11-09 10:49:40 -0600

    Save current progress before attempting merge with design

--- .godot/mono/temp/obj/Debug/Top Down Action RPG.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Top Down Action RPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Top Down Action RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23e203f4e74f16349bea803ff47d43e0e72e0eac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931cccefebaa0f936615e0fc80db31718c9d2246")]
 [assembly: System.Reflection.AssemblyProductAttribute("Top Down Action RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Top Down Action RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
