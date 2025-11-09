6fd111e303ac9636ce1240940af35de0179437db
matthehzhang <vc.matthewzhang@gmail.com>
2025-11-09 02:50:20 -0600

    abandoned house merge hopefully

--- scenes/characters/Player.gd
+++ scenes/characters/Player.gd
@@ -8,9 +8,8 @@ in the AssetLib if you want to make something more complex.
 """
 
 @export var WALK_SPEED: int = 150 # pixels per second
-@export var ROLL_SPEED: int = 300 # pixels per second
-@export var hitpoints: int = 20
-
+@export var hitpoints: int = 100
+@export var damage: int = 10
 var linear_vel = Vector2()
 var roll_direction = Vector2.DOWN
 
@@ -23,7 +22,7 @@ var despawn_fx = preload("res://scenes/misc/DespawnFX.tscn")
 var anim = ""
 var new_anim = ""
 
-enum { STATE_BLOCKED, STATE_IDLE, STATE_WALKING, STATE_ATTACK, STATE_ROLL, STATE_DIE, STATE_HURT }
+enum { STATE_BLOCKED, STATE_IDLE, STATE_WALKING, STATE_ATTACK, STATE_DIE, STATE_HURT }
 
 var state = STATE_IDLE
 
@@ -86,23 +85,9 @@ func _physics_process(_delta):
 					Input.is_action_pressed("move_up")
 				):
 					state = STATE_WALKING
-			if Input.is_action_just_pressed("attack"):
-				state = STATE_ATTACK
-			if Input.is_action_just_pressed("roll"):
-				state = STATE_ROLL
-				roll_direction = Vector2(
-						- int( Input.is_action_pressed("move_left") ) + int( Input.is_action_pressed("move_right") ),
-						-int( Input.is_action_pressed("move_up") ) + int( Input.is_action_pressed("move_down") )
-					).normalized()
-				_update_facing()
 			new_anim = "idle_" + facing
 			pass
 		STATE_WALKING:
-			if Input.is_action_just_pressed("attack"):
-				state = STATE_ATTACK
-			if Input.is_action_just_pressed("roll"):
-				state = STATE_ROLL
-			
 			set_velocity(linear_vel)
 			move_and_slide()
 			linear_vel = velocity
@@ -121,7 +106,6 @@ func _physics_process(_delta):
 			target_speed *= WALK_SPEED
 			#linear_vel = linear_vel.linear_interpolate(target_speed, 0.9)
 			linear_vel = target_speed
-			roll_direction = linear_vel.normalized()
 			
 			_update_facing()
 			
@@ -133,19 +117,6 @@ func _physics_process(_delta):
 		STATE_ATTACK:
 			new_anim = "slash_" + facing
 			pass
-		STATE_ROLL:
-			if roll_direction == Vector2.ZERO:
-				state = STATE_IDLE
-			else:
-				set_velocity(linear_vel)
-				move_and_slide()
-				linear_vel = velocity
-				var target_speed = Vector2()
-				target_speed = roll_direction
-				target_speed *= ROLL_SPEED
-				#linear_vel = linear_vel.linear_interpolate(target_speed, 0.9)
-				linear_vel = target_speed
-				new_anim = "roll"
 		STATE_DIE:
 			new_anim = "die"
 		STATE_HURT:
---
+++ scenes/levels/HouseInside.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+@export var auto_trigger_battle = true
+@export var enemy_name = "Panther"
+@export var enemy_health = 60
+@export var enemy_attack = 10
+
+var battle_triggered = false
+
+func _ready():
+	# If you have a dialog system, connect to it
+	if Dialogs and not battle_triggered:
+		# Wait a moment before starting cutscene
+		# Debugging purposes
+		print("started")
+		await get_tree().create_timer(0.5).timeout
+		if auto_trigger_battle:
+			start_encounter()
+
+func start_encounter():
+	if battle_triggered:
+		return
+	battle_triggered = true
+	
+	# Freeze player movement during cutscene
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		player.set_physics_process(false)  # Disable player movement
+	
+	# Start dialogue cutscene
+	# Adjust this based on how your Dialogs system works
+	# This is a placeholder - you'll need to adapt it to your dialog system
+	show_pre_battle_dialogue()
+
+func show_pre_battle_dialogue():
+	# If you have a Dialogs system, use it here
+	# Example dialogue lines:
+	var dialogue = "H...h..ehehehe...     Is that cat flesh I smell? Dinner time! Dinner time! DINNER! DINNERDINNERDINNERDINNER"
+
+	
+	# You'll need to adapt this to YOUR dialog system
+	# For now, I'll show a simple approach
+	await Dialogs.show_dialog(dialogue, enemy_name)
+	await get_tree().create_timer(2.0).timeout
+ 
+	# After dialogue, start battle
+	start_battle()
+
+func start_battle():
+	# Store enemy data in Globals so battle scene can access it
+	Globals.current_enemy_name = enemy_name
+	Globals.current_enemy_health = enemy_health
+	Globals.current_enemy_attack = enemy_attack
+	Globals.current_enemy_attacks = ["Pounce"]
+	Globals.battle_return_scene = "res://scenes/levels/Outside.tscn"
+	Globals.spawnpoint = "house_outside"
+	### REPLACE
+	Globals.battle_background = "res://assets/backgrounds/abandoned_house_battle"
+	
+	# Transition to battle scene
+	get_tree().change_scene_to_file("res://levels/battle.tscn")
