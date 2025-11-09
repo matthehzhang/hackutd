d2382fa90e29d62f14a817b0aa1e76ca6fc60551
matthehzhang <vc.matthewzhang@gmail.com>
2025-11-09 11:43:55 -0600

    add fights and transitions and dialogue quality

--- scenes/misc/Healthbar.gd
+++ scenes/misc/Healthbar.gd
@@ -4,20 +4,15 @@ extends HBoxContainer
 Connects to the player node and shows a health bar in the form of hearts
 """
 
-var player : Player = null
+@onready var player: Node = $"../../Player"
+signal health_changed(new_hp: int)
+var hitpoints: int = 100
+
 var heart_scene = preload("res://scenes/misc/Heart.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Try to get the player node. If null wait till next frame, rinse, repeat.
-	while (player == null):
-		var player_group = get_tree().get_nodes_in_group("player")
-		if not player_group.is_empty():
-			player = player_group.pop_front()
-		else:
-			await get_tree().idle_frame
-	
-	player.health_changed.connect(_on_health_changed)
-	_on_health_changed(player.hitpoints)
+	#player.health_changed.connect(_on_health_changed)
+	#_on_health_changed(player.hitpoints)
 	pass # Replace with function body.
 
 
